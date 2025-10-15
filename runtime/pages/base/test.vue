ad4c820522399c7b1fa173dc19530ad1c8b37900
William <williamverde10@gmail.com>
2025-10-15 16:48:29 -0400

    Update: Title in test page

--- runtime/pages/base/test.vue
+++ runtime/pages/base/test.vue
@@ -10,7 +10,7 @@ const logo = settingStore.get('isite::logo1') */
 
   <section class="container mx-auto py-10 px-4 sm:px-6 lg:px-10">
 
-    <h2 class="text-center text-3xl font-bold mb-6">Pagina de Prueba</h2>
+    <h2 class="text-center text-3xl font-bold mb-6">Pagina de Prueba desde el Modulo Ibase</h2>
 
     <div class="content">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nulla porro quae quisquam corrupti voluptatibus atque temporibus consectetur neque natus mollitia, veritatis accusamus laudantium ea numquam dolorum illum cum quaerat.
